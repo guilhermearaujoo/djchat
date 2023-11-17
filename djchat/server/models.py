fb0087c7edd56d138b3e3a6c7c796460fa113934
guilhermearaujoo <ghma.em1@gmail.com>
2023-11-17 16:49:32 -0300

    feat: create server model

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Category(models.Model):
@@ -7,3 +8,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="server_category"
+    )
+    description = models.CharField(max_length=250, null=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
