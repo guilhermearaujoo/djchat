65a5fb8a9b1405b9bfb6f8c726432d3f55ef524a
guilhermearaujoo <ghma.em1@gmail.com>
2023-11-17 16:54:49 -0300

    feat: create channel model

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -20,3 +20,22 @@ class Server(models.Model):
     )
     description = models.CharField(max_length=250, null=True)
     members = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
