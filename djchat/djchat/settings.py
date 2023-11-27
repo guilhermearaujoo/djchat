c1d34ac0d20718b4f8e88720d734492e629edac3
guilhermearaujoo <ghma.em1@gmail.com>
2023-11-27 17:02:19 -0300

    chore: add drf_espectacular (swagger)

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external
+    "drf_spectacular",
     "account",
     "server",
 ]
@@ -130,3 +132,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+}
